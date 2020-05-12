1e0a19e1d4d503907e350ec05686521288f591c8
MHHrx121299 <darkheart.hridoy@gmail.com>
2020-05-12 20:41:36 +0600

    Repositories added..

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list
+
+apt update
+
+
+apt install apt-transport-https\
+echo "deb https://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list\
+echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list\
+
+apt update
+
+
+
+
